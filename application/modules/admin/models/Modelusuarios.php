91c31f514555b835dba6d05761045fdd0d226419
neo4jbrasil <30732013+neo4jbrasil@users.noreply.github.com>
2017-11-02 16:02:00 -0200

    update
    adicionado novos models de interação com o sistema

--- application/modules/admin/models/Modelusuarios.php
+++ application/modules/admin/models/Modelusuarios.php
@@ -24,7 +24,7 @@ class Modelusuarios extends CI_Model {
         return intval($lis[0]['cont']);
     }
 	public function listar_autores(){
-        $query = 'MATCH (n:usuario) RETURN n.nome as nome, n.user as id, n.img as img ORDER BY n.nome';
+        $query = 'MATCH (n:usuario) RETURN n.nome as nome, n.user as id, n.img as img, n.ativado as ativado ORDER BY n.nome';
         $result = $this->neo4j->get_db()->run($query);
         $lis = '';
         $cont=0;
@@ -32,6 +32,7 @@ class Modelusuarios extends CI_Model {
             $lis[$cont]['nome'] = $record->value('nome');
             $lis[$cont]['id'] = $record->value('id');
             $lis[$cont]['img'] = $record->value('img');
+            $lis[$cont]['ativado'] = $record->value('ativado');
             $cont++;
 		}
         return $lis;
@@ -115,7 +116,61 @@ class Modelusuarios extends CI_Model {
 	}
 
 	public function alterar($nome,$email,$historico,$user,$senha,$img){
+		$params = [
+				'user'=>$user,
+				'novo_name'=>$nome,
+				'novo_email'=>$email,
+				'novo_historico'=>$historico,
+				'novo_senha'=>md5($senha),
+				'novo_img'=>$img
+		];
+		$query = "
+			MATCH (us:usuario)
+			where us.user = {user}
+			SET us.nome = {novo_name},us.img = {novo_img},us.historico = {novo_historico},us.email = {novo_email},us.senha = {novo_senha}
 
+			RETURN 1
+		";
+
+		if($this->neo4j->get_db()->run($query,$params)){
+			return 1;
+		}
+		return 0;
+	}
+
+	public function desativar($id){
+		$params = [
+				'user'=>$id,
+				'ativado'=>0
+		];
+		$query = "
+			MATCH (us:usuario)
+			where us.user = {user}
+			SET us.ativado = {ativado}
+			RETURN 1
+		";
+
+		if($this->neo4j->get_db()->run($query,$params)){
+			return 1;
+		}
+		return 0;
+	}
+	public function ativar($id){
+		$params = [
+				'user'=>$id,
+				'ativado'=>1
+		];
+		$query = "
+			MATCH (us:usuario)
+			where us.user = {user}
+			SET us.ativado = {ativado}
+			RETURN 1
+		";
+
+		if($this->neo4j->get_db()->run($query,$params)){
+			return 1;
+		}
+		return 0;
 	}
 }
  
