b4f8bf52af4382dc5528eb619a8f0a9c448d043d
neo4jbrasil <30732013+neo4jbrasil@users.noreply.github.com>
2017-11-02 00:45:42 -0200

    update
    atualizando string de comparação e incluido javascrit dentro da condição de chamada

--- application/modules/template_backend/views/html-footer.php
+++ application/modules/template_backend/views/html-footer.php
@@ -16,25 +16,7 @@
     <script src="http://demos.codexworld.com/includes/js/bootstrap.js"></script>
     <!-- tinymce -->
     <script src="https://cloud.tinymce.com/stable/tinymce.min.js"></script>
-    <script type="text/javascript">
-        tinymce.init({
-          selector: '#txt-conteudo',
-          height: 500,
-          theme: 'modern',
-          plugins: 'print preview fullpage powerpaste searchreplace autolink directionality advcode visualblocks visualchars fullscreen image link media template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists textcolor wordcount tinymcespellchecker a11ychecker imagetools mediaembed  linkchecker contextmenu colorpicker textpattern help',
-          toolbar1: 'formatselect | bold italic strikethrough forecolor backcolor | link | alignleft aligncenter alignright alignjustify  | numlist bullist outdent indent  | removeformat',
-          image_advtab: true,
-          templates: [
-            { title: 'Test template 1', content: 'Test 1' },
-            { title: 'Test template 2', content: 'Test 2' }
-          ],
-          content_css: [
-            '//fonts.googleapis.com/css?family=Lato:300,300i,400,400i',
-            '//www.tinymce.com/css/codepen.min.css'
-          ]
-         });    
-
-         </script>
+    
     <script type="text/javascript">
         tinymce.init({
           selector: '#txt-historico',
@@ -56,8 +38,27 @@
          </script>
         <?php 
           $metodo = $this->uri->segment(2);
-          if($metodo === 'publicacao'){
+          if($metodo === 'publicar'){
         ?>
+            <script type="text/javascript">
+              tinymce.init({
+                selector: '#txt-conteudo',
+                height: 500,
+                theme: 'modern',
+                plugins: 'print preview fullpage powerpaste searchreplace autolink directionality advcode visualblocks visualchars fullscreen image link media template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists textcolor wordcount tinymcespellchecker a11ychecker imagetools mediaembed  linkchecker contextmenu colorpicker textpattern help',
+                toolbar1: 'formatselect | bold italic strikethrough forecolor backcolor | link | alignleft aligncenter alignright alignjustify  | numlist bullist outdent indent  | removeformat',
+                image_advtab: true,
+                templates: [
+                  { title: 'Test template 1', content: 'Test 1' },
+                  { title: 'Test template 2', content: 'Test 2' }
+                ],
+                content_css: [
+                  '//fonts.googleapis.com/css?family=Lato:300,300i,400,400i',
+                  '//www.tinymce.com/css/codepen.min.css'
+                ]
+               });    
+
+             </script>
             <script src="<?php echo base_url()?>assets/js/datetimepicker/jquery.js"></script>
             <script src="<?php echo base_url()?>assets/js/datetimepicker/build/jquery.datetimepicker.full.js"></script>
             <script>
