3124f933212018ddeadfd4bfb4ae2ea9d2bcc40a
neo4jbrasil <30732013+neo4jbrasil@users.noreply.github.com>
2017-11-02 00:46:38 -0200

    update

--- application/modules/admin/views/alterar_usuario.php
+++ application/modules/admin/views/alterar_usuario.php
@@ -7,7 +7,7 @@
             </div>
             <!-- /.row -->
             <div class="row">
-                <div class="col-lg-6">
+                <div class="col-lg-12">
                     <div class="panel panel-default">
                         <div class="panel-heading">
                             Alterar <?php echo $subtitulo; ?>
@@ -15,27 +15,41 @@
                         <div class="panel-body">
                             <div class="row">
                                 <div class="col-lg-12">
+                                    <style>
+                                            img{
+                                                width: 240px;
+                                            }
+                                        </style>
                                     <?php
                                     foreach ($usuario as $key) {
-                                        echo form_open_multipart('admin/inserir_usuario');
+                                        echo form_open_multipart('admin/salvar_alteracoes_usuario');
                                         echo form_hidden('id_img',md5(uniqid(time())));
                                         $imagem = array('name'=>'userfile','id'=>'userfile','class'=>'form-control img-responsive');
                                         echo validation_errors('<div class="alert alert-danger">','</div>');
                                     ?>
-                                    <div class="form-group">
+                                    <div class="form-group col-md-6">
                                         <label>Nome do Usuário</label>
                                         <input type="texto" class="form-control" placeholder="Digite o nome completo" id="txt-nome" name="txt-nome" value="<?php echo $key['nome']; ?>">
                                     </div>
-                                    <div class="form-group">
+                                    <div class="form-group col-md-6">
                                         <label>Email</label>
                                         <input type="email" class="form-control" placeholder="Digite o email" id="txt-email" name="txt-email" value="<?php echo $key['email'];  ?>">
                                     </div>
-                                    <div class="form-group">
+                                    <div class="form-group col-md-6">
+                                        <label>Senha</label>
+                                        <input type="password" class="form-control" placeholder="Digite a Senha" id="txt-senha" name="txt-senha" >
+                                    </div>
+
+                                    <div class="form-group col-md-6">
+                                        <label>Confirme a Senha</label>
+                                        <input type="password" class="form-control" placeholder="Digite a Senha" id="txt-senha" name="txt-senha-2">
+                                    </div>
+                                    <div class="form-group col-md-6">
                                         <label>Historico</label>
                                         <textarea class="form-control" placeholder="Digite o Conteudo" id="txt-historico" name="txt-historico"><?php echo $key['historico'];  ?></textarea> 
                                     </div>
                                     
-                                    <div class="form-group">
+                                    <div class="form-group col-md-6">
 
                                         <label>Imagem de Perfil</label>
                                         <div class="col-lg-3 col-lg-offset-3">
@@ -50,22 +64,15 @@
                                         </div>
                                         <?php echo form_upload($imagem); ?>
                                     </div>
-                                    <div class="form-group">
-                                        <label>Senha</label>
-                                        <input type="password" class="form-control" placeholder="Digite a Senha" id="txt-senha" name="txt-senha" >
-                                    </div>
+                                    
 
-                                    <div class="form-group">
-                                        <label>Confirme a Senha</label>
-                                        <input type="password" class="form-control" placeholder="Digite a Senha" id="txt-senha" name="txt-senha-2">
-                                    </div>
-
-                                    <input type="hidden" name="txt-id" value="<?php echo $key['user'];  ?>">
+                                    <input type="hidden" name="txt-id" value="<?php echo $key['id'];  ?>">
 
                                     <button type="submit" class="btn btn-default">Atualizar</button>
                                     <?php
                                         
                                         echo form_close();
+                                        }
                                     ?>
                                 </div>
                                 
