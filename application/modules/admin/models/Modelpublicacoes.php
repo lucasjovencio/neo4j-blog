4f40cdec9ef9692f018eae8c1861fcedef6c02c8
neo4jbrasil <30732013+neo4jbrasil@users.noreply.github.com>
2017-11-05 20:16:38 -0200

    update
    melhorias no model

--- application/modules/admin/models/Modelpublicacoes.php
+++ application/modules/admin/models/Modelpublicacoes.php
@@ -8,31 +8,31 @@ class Modelpublicacoes extends Abstract_model{
     public function listar_publicacao($pular,$post_por_pagina){
 
         $query = '
-            MATCH (autor:usuario)-[w:publica]->(pub:publicacao)-[r:contidoEm]->(cat:categoria)
+            MATCH (autor:usuario)-[w:publica]->(pub:publicacao)
 
             RETURN autor.nome as nome, 
                 w.data as data,
                 pub.titulo as titulo, 
                 pub.subtitulo as subtitulo, 
                 pub.conteudo as conteudo, 
-                cat.name as name, 
-                id(pub) as id, 
-                pub.img as img
-                ORDER BY pub.titulo 
+                id(pub) as id,
+                pub.img as img,
+                pub.visivel as visivel
+                ORDER BY pub.data DESC 
                 SKIP {pular} 
                 LIMIT {limit}
         ';
-        $params = ['pular'=>intval($pular),'limit'=>$post_por_pagina];
+        $params = ['pular'=>intval($pular),'limit'=>intval($post_por_pagina)];
         if(($result = $this->neo4j->get_db()->run($query,$params))){
             $lis = '';
             $cont=0;
     		foreach ($result->records() as $record) {
                 $lis[$cont]['autor'] = $record->value('nome');
                 $lis[$cont]['titulo'] = $record->value('titulo');
-                $lis[$cont]['categoria'] = $record->value('name');
                 $lis[$cont]['subtitulo'] = $record->value('subtitulo');
                 $lis[$cont]['conteudo'] = $record->value('conteudo');
                 $lis[$cont]['data'] = $record->value('data');
+                $lis[$cont]['visivel'] = $record->value('visivel');
                 $lis[$cont]['id'] = $record->value('id');
                 $lis[$cont]['img'] = $record->value('img');
                 $cont++;
@@ -43,7 +43,7 @@ class Modelpublicacoes extends Abstract_model{
     }
     public function contar(){
         $query = '
-            MATCH (autor:usuario)-[w:publica]->(pub:publicacao)-[r:contidoEm]->(cat:categoria)
+            MATCH (pub:publicacao)
 
             RETURN count(pub) as id
         ';
@@ -73,6 +73,54 @@ class Modelpublicacoes extends Abstract_model{
         }
         return 0;
     }
+
+    public function single_publicacao($id){
+
+        $query = '
+            MATCH (pub:publicacao)
+            WHERE ID(pub) = {param}
+            MATCH (autor:usuario)-[w:publica]->(pub)-[r:contidoEm]->(cat:categoria), (pub)-[rr:extensaoBiblioteca]->(bib:javascript)
+                RETURN autor.nome as nome, 
+                    w.data as data,
+                    pub.titulo as titulo, 
+                    pub.subtitulo as subtitulo, 
+                    pub.conteudo as conteudo, 
+                    
+                    id(pub) as id, 
+                    pub.img as img,
+                    id(autor) as id_autor,
+                    autor.nome as nome_autor,
+                    COLLECT(distinct bib.name) as js,
+                    COLLECT(distinct bib.url) as urlbib,
+                    COLLECT(distinct cat.name) as cat
+        ';
+        $params = ['param'=>intval($id)];
+        if(($result = $this->neo4j->get_db()->run($query,$params))){
+            $lis = '';
+
+            $lis[0]['autor'] =$result->getRecord()->value('nome');
+            $lis[0]['titulo'] =$result->getRecord()->value('titulo');
+            $lis[0]['subtitulo'] =$result->getRecord()->value('subtitulo');
+            $lis[0]['conteudo'] =$result->getRecord()->value('conteudo');
+            $lis[0]['data'] =$result->getRecord()->value('data');
+            $lis[0]['id'] =$result->getRecord()->value('id');
+            $lis[0]['id_autor']=$result->getRecord()->value('id_autor');
+            $lis[0]['nome_autor']=$result->getRecord()->value('nome_autor');
+
+            $lis[0]['categoria'] =$result->getRecord()->value('cat');
+            $lis[0]['javascript'] =$result->getRecord()->value('js');
+            $lis[0]['urlJS'] =$result->getRecord()->value('urlbib');
+            if($result->getRecord()->value('img') !=null){                
+                $lis[0]['img'] =$result->getRecord()->value('img');
+            }else{
+                $lis[0]['img'] = 'null';
+            }
+
+            return $lis;
+        }
+        return null;
+    }
+    
     public function adicionar($titulo,$subtitulo,$conteudo,$date,$id,$cat,$url,$java){
         $query = "
             match (autor:usuario{user:{username}})
@@ -85,16 +133,13 @@ class Modelpublicacoes extends Abstract_model{
             MERGE (autor)-[:publica{data:{date}}]->(pub)
             return id(pub) as id
         ";
-
         $params = ['titulo'=>$titulo,'subtitulo'=>$subtitulo,'conteudo'=>$conteudo,'date'=>$date,'username'=>$id,'url'=>$url];
         if($result=$this->neo4j->get_db()->run($query, $params)){
-
             $idpub= $result->getRecord()->value("id");
             $query='';
             $result = NULL;
             $stack = $this->neo4j->get_db()->stack();
             
-
             foreach ($cat as $key) {
                 $query = "
                     match (cat:categoria{name:{categoria}}), (pub:publicacao)
@@ -117,15 +162,64 @@ class Modelpublicacoes extends Abstract_model{
                 return 1;
             }
         }
-
         return 0;
     }
-    public function alterar($name,$id){
-        $query = 'MATCH (n:categoria{ name: {param}})
-        SET n.name = {novo_name} RETURN count(n) as cont';
-        $params = ['param' => $id,'novo_name' => $name];
+    public function alterar($titulo,$subtitulo,$conteudo,$date,$id,$cat,$url,$js,$id_user){
+        $query = "
+            match (aut:usuario)-[p:publica]->(pub:publicacao)
+            where id(pub)={id} and  aut.user = {id_user}
+            match (pub)-[cat:contidoEm]->(catt:categoria), (pub)-[jst:extensaoBiblioteca]->(jss:javascript)
+            
+            SET pub.modificado = 1
+            delete jst,cat
+            
+            WITH COLLECT(distinct jss.name) as bib,
+            COLLECT(distinct catt.name) as cate,
+            aut,pub
 
-        if($this->neo4j->get_db()->run($query, $params)){
+            create (aut)-[mo:historico_mudanca{
+                titulo:pub.titulo,
+                subtitulo: pub.subtitulo,
+                data:pub.data,
+                dataModificacao:{dataMod},
+                img:pub.img,
+                javascript:bib,
+                categoria:cate,
+                visivel:1
+            }]->(pub)
+
+            SET pub.titulo = {titulo},
+                pub.subtitulo = {subtitulo},
+                pub.data = {data},
+                pub.img = {url}
+        ";
+        
+        $dateModi = new DateTime();
+        $dataM = (string)$dateModi->format('Y-m-d H:i:s');
+        $params = ['id' => intval($id),'id_user' => $id_user,'titulo'=>$titulo,'subtitulo'=>$subtitulo,'data'=>$date,'dataMod'=>$dataM,'url'=>$url];
+        $this->neo4j->get_db()->run($query, $params);
+        $stack = $this->neo4j->get_db()->stack();
+
+        foreach ($cat as $key) {
+            $query = "
+                match (cat:categoria{name:{categoria}}), (pub:publicacao)
+                where id(pub) = {idpub}
+                MERGE (pub)-[:contidoEm]->(cat)
+            ";
+            $params = ['categoria'=>$key,'idpub'=>intval($id)];
+            $stack->push($query, $params);
+        };
+        foreach ($js as $key) {
+            $query = "
+                match (jss:javascript{name:{js}}), (pub:publicacao)
+                where id(pub) = {idpub}
+                MERGE (pub)-[:extensaoBiblioteca]->(jss)
+            ";
+            $params = ['js'=>$key,'idpub'=>intval($id)];
+            $stack->push($query, $params);
+        };
+        
+        if($this->neo4j->get_db()->runStack($stack)){
             return 1;
         }
         return 0;
@@ -140,5 +234,63 @@ class Modelpublicacoes extends Abstract_model{
             return 1;
         }
         return 0;
+    }
+
+    public function categoria_to_artigo($artigo,$all_categoria){
+        $aux = '';
+        $aux2 = '';
+        $cont=0;
+        $cont2=0;
+        foreach ($artigo as $key) {
+            
+            $aux[$cont]=$key;
+
+            $cont++;
+        }
+
+        foreach ($all_categoria as $key) {
+            for ($i=0; $i < $cont ; $i++) { 
+                if($key['name']===$aux[$i]){
+                    $aux2[$cont2]['name']=$key['name'];
+                    $aux2[$cont2]['selecionado']="selected";
+                    break;
+                }
+                else{
+                    $aux2[$cont2]['name']=$key['name'];
+                    $aux2[$cont2]['selecionado']=" ";
+                }
+            }
+            $cont2++;
+        }
+        return $aux2;
+    }
+
+    public function javascript_to_artigo($artigo,$all_javascript){
+        $aux = '';
+        $aux2 = '';
+        $cont=0;
+        $cont2=0;
+        foreach ($artigo as $key) {
+            
+            $aux[$cont]=$key;
+
+            $cont++;
+        }
+        
+        foreach ($all_javascript as $key) {
+            for ($i=0; $i < $cont ; $i++) { 
+                if($key['name']===$aux[$i]){
+                    $aux2[$cont2]['name']=$key['name'];
+                    $aux2[$cont2]['selecionado']="selected";
+                    break;
+                }
+                else{
+                    $aux2[$cont2]['name']=$key['name'];
+                    $aux2[$cont2]['selecionado']=" ";
+                }
+            }
+            $cont2++;
+        }
+        return $aux2;
     }
 }
